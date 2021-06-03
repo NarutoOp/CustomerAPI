12b170138b98039ccb2713c386b7ec971343d81a
Arpit Gupta <zingahugane@gmail.com>
2021-06-03 17:22:06 +0530

    Added API Project

---
+++ CustomerAPI/Models/CustomerContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.Models
+{
+    public class CustomerContext:DbContext
+    {
+        public CustomerContext(DbContextOptions opts) : base(opts) { }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasData( new Customer(){
+                Id = 1,
+                Name = "Ken",
+                Age = 21
+            });
+        }
+    }
+}
